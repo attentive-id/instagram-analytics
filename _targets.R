6c98774cd79c49cdda9ab584c3e725904642fc83
lamurian <lamuri.aly8@gmail.com>
2024-10-19 19:47:59 +0200

    Complete the VAR model requirement

--- _targets.R
+++ _targets.R
@@ -53,6 +53,31 @@ list(
   # Perform an ADF test to evaluate the unit root
   tar_target(res_adf, evalUnitRoot(ts_reg)),
 
+  # Fit a VAR model and identify the structural impact matrix
+  tar_target(mod_var, fitVAR(ts_reg, cum_daily_Follows, daily_Reach, daily_Visits)),
+  tar_target(mod_svar, identifySVAR(mod_var, stage3 = TRUE)),
+
+  # Calculate and plot the imppulse response function
+  tar_target(mod_irf_var, vars::irf(mod_var, runs = 1e3, seed = seed)),
+  tar_target(fig_irf_var, saveFig(mod_irf_var, vars:::plot.varirf, file = "docs/figs/plt-var-irf.pdf", plot.type = "multiple")),
+
+  tar_target(mod_irf_svar, svars::wild.boot(mod_svar, n.ahead = 14, nboot = 1e3, nc = 4)),
+  tar_target(plt_irf_svar, svars:::plot.sboot(mod_irf_svar, scales = "free_y")),
+  tar_target(fig_irf_svar, ggplot2::ggsave(plt_irf_svar, file = "docs/figs/plt-svar-irf.pdf", height = 8, width = 12)),
+
+  # Calculate and plot the forecast error variance decomposition
+  tar_map(
+    unlist = FALSE,
+    values = tibble::tibble(
+      mod = rlang::syms(c("mod_var", "mod_svar")),
+      fname = c("var", "svar")
+    ),
+    names = "fname",
+    tar_target(mod_fevd, getFEVD(mod)),
+    tar_target(plt_fevd, svars:::plot.svarfevd(mod_fevd)),
+    tar_target(fig_fevd, ggplot2::ggsave(plt_fevd, file = sprintf("docs/figs/plt-%s-fevd.pdf", fname), height = 8 , width = 12))
+  ),
+
   # Generate documentation
   tar_quarto(readme, "README.qmd", priority = 0)
 
