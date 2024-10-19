f97b7eed7e8792cfdda20beb99332b9f86473e72
lamurian <lamuri.aly8@gmail.com>
2024-10-19 19:47:30 +0200

    Save generic plot produced by base::plot

--- src/R/visualize.R
+++ src/R/visualize.R
@@ -21,3 +21,25 @@ vizPair <- function(ts, pattern = "Follows") {
   return(plt)
 }
 
+saveFig <- function(obj, FUN = NULL, file, ...) {
+  #' Save Figure
+  #'
+  #' Write figure from a generic R objectg as a pdf file.
+  #'
+  #' @param obj A generic R object to plot
+  #' @param FUN The function for plotting, will call `base::plot` when set to
+  #' null
+  #' @param file The file name for saving the figure
+  #' @param ... Parameters being passed on to `FUN`
+
+  pdf(file, height = 8, width = 10)
+  
+  if (is.null(FUN)) {
+    base::plot(obj, ...)
+  } else {
+    FUN(obj, ...)
+  }
+
+  dev.off()
+
+}
